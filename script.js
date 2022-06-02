5d98d61494e657f2db1177d320b57561f3a84bd9
Albertshch <50829692+Albertshch@users.noreply.github.com>
2022-06-02 15:14:13 +0300

    1234

---
+++ script.js
@@ -0,0 +1,15 @@
+// function show_modal() {
+//     document.querySelector('[data-target="#exampleModal"]').click();
+//   }
+
+function viewDiv(){
+    document.getElementById("div1").style.display = "block";
+  };
+
+
+  function viewDiv2(){
+    document.getElementById("div2").style.display = "block";
+  };
+
+
+  
